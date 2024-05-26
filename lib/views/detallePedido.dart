c0a72e71abd24e9765687badb7c6643b1153caa7
paula-alonso <paula.alonso@correo.unimet.edu.ve>
2024-05-26 11:19:21 -0400

    refactor (background image): widget with the universal background ♻️

--- lib/views/detallePedido.dart
+++ lib/views/detallePedido.dart
@@ -1,4 +1,5 @@
 import 'package:easyorder/views/Widgets/Product_card.dart';
+import 'package:easyorder/views/Widgets/background_image.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -14,160 +15,159 @@ class _detallePedidoState extends State<detallePedido> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          backgroundColor: Color.fromARGB(0, 255, 255, 255),
-          scrolledUnderElevation: 0,
-        ),
-        body: Stack(children: [
-          Positioned(
-            top: -80,
-            right: -60,
-            child: Transform.rotate(
-              angle: 0.3,
-              child: Opacity(
-                opacity: 0.3,
-                child: Image.asset(
-                  'images/fruits.png',
-                  width: 480,
-                  height: 245,
-                  fit: BoxFit.cover,
+      backgroundColor: Colors.white,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(0, 255, 255, 255),
+        scrolledUnderElevation: 0,
+      ),
+      body: Background_image(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: kToolbarHeight + 40),
+              Text(
+                'Detalles del pedido',
+                style: GoogleFonts.poppins(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: kToolbarHeight + 40),
-                Text(
-                  'Detalles del pedido',
-                  style: GoogleFonts.poppins(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  ),
+              Gap(20),
+              Expanded(
+                child: ListView(
+                  padding: EdgeInsets.zero,
+                  children: [
+                    ProductCard(
+                      productName: 'Hamburguesa',
+                      productPrice: 19.8,
+                      productImage:
+                          'https://recetasdeusa.com/wp-content/uploads/2022/05/Hamburguesa-americana-1-scaled.jpg',
+                    ),
+                    Gap(20),
+                    ProductCard(
+                      productName: 'Hamburguesa',
+                      productPrice: 19.8,
+                      productImage:
+                          'https://recetasdeusa.com/wp-content/uploads/2022/05/Hamburguesa-americana-1-scaled.jpg',
+                    ),
+                    Gap(20),
+                    ProductCard(
+                      productName: 'Hamburguesa',
+                      productPrice: 19.8,
+                      productImage:
+                          'https://recetasdeusa.com/wp-content/uploads/2022/05/Hamburguesa-americana-1-scaled.jpg',
+                    ),
+                  ],
                 ),
-                Gap(20),
-                Expanded(
-                  child: ListView(
-                    padding: EdgeInsets.zero,
-                    children: [
-                      ProductCard(
-                        productName: 'Hamburguesa',
-                        productPrice: 19.8,
-                        productImage:
-                            'https://recetasdeusa.com/wp-content/uploads/2022/05/Hamburguesa-americana-1-scaled.jpg',
-                      ),
-                    ],
-                  ),
-                ),
-                Gap(40),
-                Container(
-                  padding: EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(7),
-                      gradient: const LinearGradient(
-                        colors: [
-                          Color.fromRGBO(255, 95, 4, 1),
-                          Colors.red,
-                        ],
-                      )),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Sub-Total:',
-                            style: GoogleFonts.poppins(
-                              fontSize: 16,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            '1111',
-                            style: GoogleFonts.poppins(
-                              fontSize: 16,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          )
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Descuento:',
-                            style: GoogleFonts.poppins(
-                              fontSize: 16,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            '0',
-                            style: GoogleFonts.poppins(
-                              fontSize: 16,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          )
-                        ],
-                      ),
-                      Gap(10),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Total:',
-                            style: GoogleFonts.roboto(
-                              fontSize: 18,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            '1111',
-                            style: GoogleFonts.roboto(
-                              fontSize: 18,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          )
-                        ],
-                      ),
-                      Gap(10),
-                      Container(
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.white,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(7),
-                            ),
-                          ),
-                          child: Text(
-                            'Ordenar',
-                            style: GoogleFonts.poppins(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              color: Color.fromRGBO(255, 95, 4, 1),
-                            ),
+              ),
+              Gap(40),
+              Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(7),
+                    gradient: const LinearGradient(
+                      colors: [
+                        Color.fromRGBO(255, 95, 4, 1),
+                        Colors.red,
+                      ],
+                    )),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Sub-Total:',
+                          style: GoogleFonts.poppins(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ),
+                        Text(
+                          '1111',
+                          style: GoogleFonts.poppins(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Descuento:',
+                          style: GoogleFonts.poppins(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          '0',
+                          style: GoogleFonts.poppins(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ],
+                    ),
+                    Gap(10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Total:',
+                          style: GoogleFonts.roboto(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          '1111',
+                          style: GoogleFonts.roboto(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ],
+                    ),
+                    Gap(10),
+                    Container(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(7),
+                          ),
+                        ),
+                        child: Text(
+                          'Ordenar',
+                          style: GoogleFonts.poppins(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromRGBO(255, 95, 4, 1),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
           ),
-        ]));
+        ),
+      ),
+    );
   }
 }
--- lib/views/menu.dart
+++ lib/views/menu.dart
@@ -1,4 +1,5 @@
 import 'package:easyorder/views/Widgets/Product_Card_Init.dart';
+import 'package:easyorder/views/Widgets/background_image.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:gap/gap.dart';
@@ -12,13 +13,13 @@ class Menu extends StatefulWidget {
 }
 
 class _MenuState extends State<Menu> {
-  String infoQr="";
+  String infoQr = "";
 
-  int botonIndice=0;
-  List <int> botones=[0,1,2]; //se deberia tener un numero por cada categoria
-  Color colorBoton1=Color(0xFFFF5F04);
+  int botonIndice = 0;
+  List<int> botones = [0, 1, 2]; //se deberia tener un numero por cada categoria
+  Color colorBoton1 = Color(0xFFFF5F04);
   //Color colorBoton2=Colors.white;
-    @override
+  @override
   void initState() {
     super.initState();
     infoQr = widget.info;
@@ -38,7 +39,7 @@ class _MenuState extends State<Menu> {
       //        height: double.infinity,
       //        decoration: BoxDecoration(
       //          image: DecorationImage(
-      //            image: AssetImage('images/fruits.png'), 
+      //            image: AssetImage('images/fruits.png'),
       //            fit: BoxFit.cover,
       //            colorFilter: ColorFilter.mode(Colors.white.withOpacity(0.4), BlendMode.dstATop),
       //          ),
@@ -56,154 +57,157 @@ class _MenuState extends State<Menu> {
       //  ),
       //),
       appBar: AppBar(
-          backgroundColor: Color.fromARGB(0, 255, 255, 255),
-          scrolledUnderElevation: 0,
-        ),
-      body: Stack(
-        children: [
-          Positioned(
-            top: -80,
-            right: -60,
-            child: Transform.rotate(
-              angle: 0.3,
-              child: Opacity(
-                opacity: 0.3,
-                child: Image.asset(
-                  'images/fruits.png',
-                  width: 480,
-                  height: 245,
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
+        backgroundColor: Color.fromARGB(0, 255, 255, 255),
+        scrolledUnderElevation: 0,
+      ),
+      body: Background_image(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(height: kToolbarHeight + 40),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: Text("Menú del Restaurante", style: GoogleFonts.poppins(fontSize: 20, fontWeight: FontWeight.bold,)),
+                child: Text(
+                  "Menú del Restaurante",
+                  style: GoogleFonts.poppins(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
               Gap(20),
               Container(
-                width: double.infinity, 
+                width: double.infinity,
                 height: 50,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-
-                  children: [
-                    Row(
+                child: ListView(scrollDirection: Axis.horizontal, children: [
+                  Row(
                     children: [
-                      SizedBox( width: 15,),
+                      SizedBox(
+                        width: 15,
+                      ),
                       OutlinedButton(
-                        onPressed: (){
+                        onPressed: () {
                           setState(() {
-                            botonIndice=0;
+                            botonIndice = 0;
                           });
-                        }, 
+                        },
                         style: OutlinedButton.styleFrom(
-                          backgroundColor: botonIndice==0? Color(0xFFFF5F04):Colors.white,
-                          foregroundColor: botonIndice==0? Colors.white:Colors.black,
+                          backgroundColor: botonIndice == 0
+                              ? Color(0xFFFF5F04)
+                              : Colors.white,
+                          foregroundColor:
+                              botonIndice == 0 ? Colors.white : Colors.black,
                           shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10)  
-                            ),
-                          minimumSize: Size(45, 40),   
+                              borderRadius: BorderRadius.circular(10)),
+                          minimumSize: Size(45, 40),
                         ),
-                        child: Text("Parrilla Guanteña"), 
-                        ),
-                      SizedBox(width: 10,),
-                      OutlinedButton(onPressed: (){
-                        setState(() {
-                          botonIndice=1;
-                        });
-                      }, 
-                      style: OutlinedButton.styleFrom(
-                        backgroundColor: botonIndice==1? Color(0xFFFF5F04):Colors.white,
-                        foregroundColor: botonIndice==1? Colors.white:Colors.black ,
-                        shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)  
-                        ),
-                        minimumSize: Size(45, 40),   
+                        child: Text("Parrilla Guanteña"),
                       ),
-                      child: Text("Parrilla Caraqueña")),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      OutlinedButton(
+                          onPressed: () {
+                            setState(() {
+                              botonIndice = 1;
+                            });
+                          },
+                          style: OutlinedButton.styleFrom(
+                            backgroundColor: botonIndice == 1
+                                ? Color(0xFFFF5F04)
+                                : Colors.white,
+                            foregroundColor:
+                                botonIndice == 1 ? Colors.white : Colors.black,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            minimumSize: Size(45, 40),
+                          ),
+                          child: Text("Parrilla Caraqueña")),
                       SizedBox(width: 10),
-                      OutlinedButton(onPressed: (){
-                        setState(() {
-                          botonIndice=2;
-                        });
-                      }, 
-                      style: OutlinedButton.styleFrom(
-                        backgroundColor: botonIndice==2? Color(0xFFFF5F04):Colors.white,
-                        foregroundColor: botonIndice==2? Colors.white:Colors.black ,
-                        shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)  
-                        ),
-                        minimumSize: Size(45, 40),   
+                      OutlinedButton(
+                          onPressed: () {
+                            setState(() {
+                              botonIndice = 2;
+                            });
+                          },
+                          style: OutlinedButton.styleFrom(
+                            backgroundColor: botonIndice == 2
+                                ? Color(0xFFFF5F04)
+                                : Colors.white,
+                            foregroundColor:
+                                botonIndice == 2 ? Colors.white : Colors.black,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            minimumSize: Size(45, 40),
+                          ),
+                          child: Text("Postre")),
+                      SizedBox(
+                        width: 10,
                       ),
-                      child: Text("Postre")),
-                      SizedBox(width: 10,),
                     ],
                   ),
                 ]),
               ),
-              SizedBox(height: 10,),
+              SizedBox(
+                height: 10,
+              ),
               Expanded(
                 child: ListView(
                   padding: EdgeInsets.zero,
                   children: [
-                    botonIndice==0?
-                      ProductCardInit(
-                      productName: "Parrilla con platano", 
-                      productPrice: 15.8, 
-                      productImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZs4XdR6VDF8inuMgk5_rLDBdQF7pVv4-b6Y63nyUF0g&s"
-                      )
-                    : botonIndice==1? 
-                      ProductCardInit(
-                      productName: "Parrilla sin platano", 
-                      productPrice: 14.0, 
-                      productImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI0vuP2m3JkbwjczFfZwIxqAy8Ub55p1lw7rtSiREp1A&s")
-                    : ProductCardInit(
-                      productName: "Quesillo", 
-                      productPrice: 6.0, 
-                      productImage: "https://mmedia.estampas.com/18856/quesillo-sin-huequitos-81792.jpg")
-                ],
-              ),
+                    botonIndice == 0
+                        ? ProductCardInit(
+                            productName: "Parrilla con platano",
+                            productPrice: 15.8,
+                            productImage:
+                                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZs4XdR6VDF8inuMgk5_rLDBdQF7pVv4-b6Y63nyUF0g&s")
+                        : botonIndice == 1
+                            ? ProductCardInit(
+                                productName: "Parrilla sin platano",
+                                productPrice: 14.0,
+                                productImage:
+                                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI0vuP2m3JkbwjczFfZwIxqAy8Ub55p1lw7rtSiREp1A&s")
+                            : ProductCardInit(
+                                productName: "Quesillo",
+                                productPrice: 6.0,
+                                productImage:
+                                    "https://mmedia.estampas.com/18856/quesillo-sin-huequitos-81792.jpg")
+                  ],
+                ),
               ),
             ],
-            ),
-          ),]
-      ),
-    bottomNavigationBar: BottomNavigationBar(
-      backgroundColor: Colors.white,
-      fixedColor: Color.fromRGBO(142, 142, 142, 1),
-      selectedLabelStyle: GoogleFonts.poppins(
-        fontWeight: FontWeight.bold,
-      ),
-      unselectedLabelStyle: GoogleFonts.poppins(
-        fontWeight: FontWeight.bold,
-      ),
-      items: const [
-        BottomNavigationBarItem(
-          icon: Icon(
-            Icons.home,
-            size: 45.0,
-            color: Color.fromRGBO(255, 95, 4, 1),
-            ),
-          label: 'Home',
           ),
-        BottomNavigationBarItem(
-          icon: Icon(
-            Icons.qr_code,
-            size: 45.0,
-            color: Color.fromRGBO(255, 95, 4, 1),
-            ),
-          label: "Scan",
-          )
-      ]
+        ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          fixedColor: Color.fromRGBO(142, 142, 142, 1),
+          selectedLabelStyle: GoogleFonts.poppins(
+            fontWeight: FontWeight.bold,
+          ),
+          unselectedLabelStyle: GoogleFonts.poppins(
+            fontWeight: FontWeight.bold,
+          ),
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                size: 45.0,
+                color: Color.fromRGBO(255, 95, 4, 1),
+              ),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.qr_code,
+                size: 45.0,
+                color: Color.fromRGBO(255, 95, 4, 1),
+              ),
+              label: "Scan",
+            )
+          ]),
     );
   }
 }
