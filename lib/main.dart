eb7c3d2508d667e7641957c0f17cbfe746455e09
paula-alonso <paula.alonso@correo.unimet.edu.ve>
2024-05-25 21:23:46 -0400

    feat(shopping cart): add the total button ✨

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: detallePedido() ,
-    );
+    return MaterialApp();
   }
 }
