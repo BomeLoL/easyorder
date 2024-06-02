6c5c09b80707f95328c7171c7d054a5ce932d5ab
Nakami <natalia.rivas@correo.unimet.edu.ve>
2024-06-01 21:28:34 -0400

    fix: error view

--- lib/views/Widgets/scanner_error_widget.dart
+++ lib/views/Widgets/scanner_error_widget.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
-import 'package:easyorder/views/escaneoQR.dart';
+import 'package:permission_handler/permission_handler.dart';
+
 
 class ScannerErrorWidget extends StatelessWidget {
   const ScannerErrorWidget({super.key, required this.error});
@@ -23,7 +24,9 @@ class ScannerErrorWidget extends StatelessWidget {
         errorMessage = 'Error';
         break;
     }
-    return ColoredBox(
+
+    if (errorMessage=='No se obtuvo permiso para usar la cámara') {
+      return ColoredBox(
       color: Colors.black,
       child: Center(
         child: Column(
@@ -41,26 +44,73 @@ class ScannerErrorWidget extends StatelessWidget {
               error.errorDetails?.message ?? '',
               style: const TextStyle(color: Colors.white),
             ),
-            ElevatedButton(
-            onPressed:(){
-              Navigator.pop(context);
-              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context){return Escanear();} ));
-              },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.black,
-            foregroundColor: Colors.white,
-              shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10)
-              ),
-            minimumSize: Size(100, 40),
-            ), 
-            child: Text("Volver",
-                style: GoogleFonts.poppins(fontSize: 20, fontWeight: FontWeight.bold),
-            )
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                onPressed:(){
+                  Navigator.pop(context);
+                  },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)
+                  ),
+                minimumSize: Size(60, 30),
+                ), 
+                child: Text("Cancelar",
+                    style: GoogleFonts.poppins(fontSize: 17, fontWeight: FontWeight.bold),
+                )
+                ),
+                ElevatedButton(
+                onPressed:(){
+                  openAppSettings();
+                  Navigator.pop(context);
+                  },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)
+                  ),
+                minimumSize: Size(60, 30),
+                ), 
+                child: Text("Otorgar permiso",
+                    style: GoogleFonts.poppins(fontSize: 17, fontWeight: FontWeight.bold),
+                )
+                ),
+              ],
             )
           ],
         ),
       ),
     );
+    } else {
+      return ColoredBox(
+      color: Colors.black,
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(bottom: 16),
+              child: Icon(Icons.error, color: Colors.white),
+            ),
+            Text(
+              errorMessage,
+              style: const TextStyle(color: Colors.white),
+            ),
+            Text(
+              error.errorDetails?.message ?? '',
+              style: const TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+    }
+    
   }
 }
