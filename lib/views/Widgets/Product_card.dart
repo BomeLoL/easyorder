b9f22ee7ea49cf700a05b713ddf904a61ba090cd
mginez <mginez@correo.unimet.edu.ve>
2024-06-02 17:45:46 -0400

    refactor: Popup text on Product Card modified

--- lib/views/Widgets/Product_card.dart
+++ lib/views/Widgets/Product_card.dart
@@ -216,41 +216,52 @@ class ProductCard extends StatelessWidget {
                             content: Text(
                                 'Al seleccionar confirmar se eliminarán todos los productos del tipo "${producto.nombreProducto}" del carrito'),
                             actions: [
-                              ElevatedButton(
-                                  style: ElevatedButton.styleFrom(
-                                      backgroundColor: Colors.transparent,
-                                      shadowColor: Colors.transparent,
-                                      elevation: 0,
-                                      shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(7))),
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                  child: Text(
-                                    'Cancelar',
-                                    style: GoogleFonts.poppins(
-                                      color: const Color.fromRGBO(255, 95, 4, 1),
-                                      fontWeight: FontWeight.bold),
-                                    )),
-                              ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                    backgroundColor:
-                                        const Color.fromRGBO(255, 95, 4, 1),
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(7))),
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                  cartController.deleteProducts(
-                                      producto, info, context, isPedido);
-                                },
-                                child: Text(
-                                  'Confirmar',
-                                  style: GoogleFonts.poppins(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold),
-                                ),
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                            backgroundColor: Colors.transparent,
+                                            shadowColor: Colors.transparent,
+                                            elevation: 0,
+                                            shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(7))),
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: Text(
+                                          'Cancelar',
+                                          style: GoogleFonts.poppins(
+                                            color: const Color.fromRGBO(255, 95, 4, 1),
+                                            fontWeight: FontWeight.bold),
+                                            textScaler: TextScaler.linear(0.9),
+                                            
+                                          )),
+                                  ),
+                                  Expanded(
+                                    child: ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                          backgroundColor:
+                                              const Color.fromRGBO(255, 95, 4, 1),
+                                          shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(7))),
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                        cartController.deleteProducts(
+                                            producto, info, context, isPedido);
+                                      },
+                                      child: Text(
+                                        'Confirmar',
+                                        style: GoogleFonts.poppins(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold),
+                                            textScaler: TextScaler.linear(0.9),
+                                      ),
+                                    ),
+                                  ),
+                                ],
                               ),
                             ]);
                       },
