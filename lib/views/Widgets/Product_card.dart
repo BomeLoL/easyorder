1c24da8647253b4b6916b5dc1f219c1de43da12e
paula-alonso <paula.alonso@correo.unimet.edu.ve>
2024-06-02 16:12:36 -0400

    feat: delete products popup ✨

--- lib/views/Widgets/Product_card.dart
+++ lib/views/Widgets/Product_card.dart
@@ -1,5 +1,6 @@
 import 'package:easyorder/controllers/cart_controller.dart';
 import 'package:easyorder/models/clases/item_menu.dart';
+import 'package:easyorder/views/Widgets/custom_popup.dart';
 import 'package:easyorder/views/detalleProducto.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -37,7 +38,6 @@ class ProductCard extends StatelessWidget {
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(7),
               color: Colors.white,
-              
               boxShadow: [
                 BoxShadow(
                   color: Colors.black.withOpacity(0.05),
@@ -111,8 +111,10 @@ class ProductCard extends StatelessWidget {
                 Spacer(
                   flex: 1,
                 ),
-                Consumer<CartController>(builder: (context, cartController, child) {
-                  final productoPedido = cartController.pedido.productos[producto];
+                Consumer<CartController>(
+                    builder: (context, cartController, child) {
+                  final productoPedido =
+                      cartController.pedido.productos[producto];
                   if (productoPedido != null) {
                     return Expanded(
                       flex: 8,
@@ -123,7 +125,8 @@ class ProductCard extends StatelessWidget {
                             child: Container(
                               child: IconButton(
                                 onPressed: () {
-                                  cartController.deleteProduct(producto, info, context, isPedido);
+                                  cartController.deleteProduct(
+                                      producto, info, context, isPedido);
                                 },
                                 icon: Icon(Icons.remove),
                                 style: IconButton.styleFrom(
@@ -160,7 +163,8 @@ class ProductCard extends StatelessWidget {
                                 },
                                 icon: Icon(Icons.add),
                                 style: IconButton.styleFrom(
-                                    backgroundColor: Color.fromRGBO(255, 95, 4, 1),
+                                    backgroundColor:
+                                        Color.fromRGBO(255, 95, 4, 1),
                                     shape: RoundedRectangleBorder(
                                       borderRadius: BorderRadius.circular(7),
                                     )),
@@ -196,35 +200,72 @@ class ProductCard extends StatelessWidget {
               ],
             ),
           ),
-          if (isPedido != 1) 
-          Consumer<CartController>(
-            builder: (context, cartController, child) {
+          if (isPedido != 1)
+            Consumer<CartController>(builder: (context, cartController, child) {
               return Positioned(
-                top: 0,
-                right: 0,
-                child: Container(
-                  width: 30,
-                  height: 30,
-                  child: IconButton(
-                    onPressed: () {
-                      cartController.deleteProducts(producto, info, context, isPedido);
-                    }, 
-                    icon: Icon(
-                      Icons.close,
-                      size: 22,
-                      color: Colors.red,
+                  top: 0,
+                  right: 0,
+                  child: Container(
+                    width: 30,
+                    height: 30,
+                    child: IconButton(
+                      onPressed: () {
+                        showCustomPopup(
+                            context: context,
+                            title: '¿Eliminar todos?',
+                            content: Text(
+                                'Al seleccionar confirmar se eliminarán todos los productos del tipo "${producto.nombreProducto}" del carrito'),
+                            actions: [
+                              ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.transparent,
+                                      shadowColor: Colors.transparent,
+                                      elevation: 0,
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(7))),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Text(
+                                    'Cancelar',
+                                    style: GoogleFonts.poppins(
+                                      color: const Color.fromRGBO(255, 95, 4, 1),
+                                      fontWeight: FontWeight.bold),
+                                    )),
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor:
+                                        const Color.fromRGBO(255, 95, 4, 1),
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(7))),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                  cartController.deleteProducts(
+                                      producto, info, context, isPedido);
+                                },
+                                child: Text(
+                                  'Confirmar',
+                                  style: GoogleFonts.poppins(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ]);
+                      },
+                      icon: const Icon(
+                        Icons.close,
+                        size: 22,
+                        color: Colors.red,
                       ),
-                    style: IconButton.styleFrom(
-                      padding: EdgeInsets.zero,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(7)
-                      )
+                      style: IconButton.styleFrom(
+                          padding: EdgeInsets.zero,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(7))),
                     ),
-                    ),
-                )
-              );
-            }
-          ),
+                  ));
+            }),
         ],
       ),
     );
