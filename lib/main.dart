709f8e31786cfbde5901d0e45b3251495a0c89ef
paula-alonso <paula.alonso@correo.unimet.edu.ve>
2024-06-19 01:58:48 -0400

    Fixed issue #7: sing in with google

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,10 @@
-import 'package:easyorder/controllers/navigation_controller.dart';
 import 'package:easyorder/controllers/pedido_controller.dart';
 import 'package:easyorder/controllers/menu_edit_controller.dart';
 import 'package:easyorder/controllers/user_controller.dart';
 import 'package:easyorder/firebase_options.dart';
-import 'package:easyorder/models/clases/menu.dart';
 import 'package:easyorder/models/dbHelper/Enviroment.dart';
 import 'package:easyorder/models/dbHelper/mongodb.dart';
-import 'package:easyorder/views/escaneoQR.dart';
-import 'package:easyorder/views/qrMesa.dart';
-import 'package:easyorder/views/registro_mesa.dart';
 import 'package:easyorder/views/splashView.dart';
-import 'package:easyorder/views/vistaMesas.dart';
-import 'package:easyorder/views/vistaQr.dart';
-import 'package:easyorder/views/walletView.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
@@ -20,7 +12,6 @@ import 'package:provider/provider.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:easyorder/controllers/text_controller.dart';
-import 'package:easyorder/views/menu.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
